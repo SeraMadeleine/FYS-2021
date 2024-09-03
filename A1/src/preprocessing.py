0f2811f30ca18e2b975ab0a35badd00b1bba99c4
SeraMadeleine <sel063@uit.no>
2024-09-03 16:07:37 +0200

    added function to save plots to own file

--- A1/src/preprocessing.py
+++ A1/src/preprocessing.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+import os
 
 
 class DataProcessing(): 
@@ -29,17 +30,37 @@ class DataProcessing():
         print(f"Training set size: {len(X_train)}, Testing set size: {len(X_test)}")
         return X_train, X_test, y_train, y_test
     
+    def save_plot(self, plot_title):
+        # Save the plot in the plots directory with the name of the plot
+        plot_filename = f"{plot_title.replace(' ', '_').lower()}.png"
+        plot_directory = "../plots"
+
+        # Create the directory if it doesn't exist 
+        if not os.path.exists(plot_directory):
+            os.makedirs(plot_directory)
+
+        plt.savefig(os.path.join(plot_directory, plot_filename))
+        print(f"Plot saved to {plot_directory}/{plot_filename}")
+
     def plot_data(self):
+        plot_title = 'Liveness vs Loudness by Genre'
+
         pop_data = self.data[self.data['genre'] == 1]
         classical_data = self.data[self.data['genre'] == 0]
         plt.scatter(pop_data['liveness'], pop_data['loudness'], color='purple', edgecolor='black', label='Pop', alpha=0.7, s=50)
         plt.scatter(classical_data['liveness'], classical_data['loudness'], color='pink', edgecolor='black', label='Classical', alpha=0.7, s=50)
-        plt.title('Liveness vs Loudness by Genre', fontsize=15)
+        plt.title(plot_title, fontsize=15)
         plt.xlabel('Liveness', fontsize=12)
         plt.ylabel('Loudness', fontsize=12)
         plt.legend()
-        plt.grid(True)  # Adding a grid
+        plt.grid(True)  
+
+        # Save the plot
+        self.save_plot(plot_title)
+
         plt.show()
+
+
 
 
 
